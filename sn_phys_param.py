d8c407b0a426156d29c5cdb527a4521945cb2593
Emma Walker <emma.s.walker@gmail.com>
2014-03-15 19:29:01 -0400

    Created classes for input data

--- sn_phys_param.py
+++ sn_phys_param.py
@@ -21,6 +21,32 @@ class Supernova():
     lc_max = float('nan')
     lc_max_err = float('nan')
     
+class Bolometric(Supernova):
+    
+    __doc__="Bolometric sub-class of Supernova"
+    
+    luminosity = float('nan')
+    luminosity_err = float('nan')
+    
+    def mag_to_bol(lc_max, lc_max_err):
+
+            #turn bolometric mag to luminosity (ergs/s)
+            luminosity = 3.839e33 * 10.0**((4.75 - lc_max)/2.5)
+            luminosity_err = luminosity * 0.05
+            #DO CALC PROPERLY
+            
+            return
+            
+class Monochromatic(Supernova):
+    
+    __doc__ = "Monochromatic LC sub-class"
+
+    luminosity = float('nan')
+    luminosity_err = float('nan')
+    
+    #ADD CALCULATION
+
+
 def main():
     
     parser = argparse.ArgumentParser(description='SN Physical Parameter Analogues')
