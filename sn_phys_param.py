eb56cce45f64139e1950d62f4ce1344c3643b219
Emma Walker <emma.s.walker@gmail.com>
2014-03-17 16:04:05 -0400

    Added ejecta mass, kinetic energy and nickel mass functions. Return (val,err) tuple in each case

--- sn_phys_param.py
+++ sn_phys_param.py
@@ -86,7 +86,69 @@ class Monochromatic(Supernova):
     
         return
         
+def ejecta_mass(new, comp):
     
+    mass = new.lc_width**2 * new.velocity / (comp.lc_width**2 * comp.velocity) * comp.mej
+    
+    e1 = (2 * new.lc_width / comp.lc_width**2 * new.velocity / comp.velocity * comp.mej)**2 * new.lc_width_err**2
+	e2 = (-2.0 * new.lc_width**2 / comp.lc_width**3 * new.velocity / comp.velocity)**2 * comp.lc_width_err**2
+	e3 = ( new.lc_width**2 / comp.lc_width**2 * new.velocity / comp.velocity)**2 * comp.mej_err**2
+	#velocity terms
+	e4 = ((new.lc_width/comp.lc_width)**2 * 1./comp.velocity * comp.mej)**2 * new.velocity_err**2
+	e5 = ((new.lc_width/comp.lc_width)**2 * (new.velocity/(comp.velocity)**2)*comp.mej)**2 * comp.velocity_err**2
+	error = np.sqrt(e1 + e2 + e3 + e4 + e5)
+	return (mass, error)
+    
+def kinetic_energy(new, comp):
+    
+	ke = (new.lc_width / comp.lc_width)**2 * (new.velocity / comp.velocity)**3 * comp.ke
+	e1 = (2. * new.lc_width / comp.lc_width**2 * (new.velocity / comp.velocity)**3 * comp.ke)**2 * new.lc_width_err**2
+	e2 = (-2. * new.lc_width**2 / comp.lc_width**3 * (new.velocity / comp.velocity)**3 * comp.ke)**2 * comp.lc_width_err**2
+	e3 = ( new.lc_width**2 / comp.lc_width**2 * (new.velocity / comp.velocity)**3) **2 * comp.ke_err**2
+	
+	#velocity terms
+	
+	e4 = ((new.lc_width/comp.lc_width)**2 * 3./comp.velocity * (new.velocity/comp.velocity)**2.*comp.ke)**2 * comp.velocity_err**2
+	
+	e5 = ((new.lc_width/comp.lc_width)**2 * -3.0/new.velocity * (new.velocity/(comp.velocity)**4)*comp.ke)**2 * comp.velocity_err**2
+	
+	error = np.sqrt(e1 + e2 + e3 + e4 + e5)
+	return (ke, error)
+    
+def nickel_mass(new, comp):
+    
+	tau_ni = 6.08 / np.log(2)
+	tau_co = 77.23 / np.log(2)
+
+	eni = 1.7
+	eco = 3.67
+
+	kA = eni / tau_ni
+	kB = eco / tau_co * tau_co / (tau_co - tau_ni)
+
+	f1 = kA * np.exp(-new.rise / tau_ni) + kB * (np.exp(-new.rise / tau_co) - np.exp(-new.rise/ tau_ni))
+
+	f2 = kA * np.exp(-comp.rise / tau_ni )+ kB * (np.exp(-comp.rise / tau_co) - np.exp(-comp.rise/ tau_ni))
+
+
+	mass = comp.ni * new.luminosity / comp.luminosity  * f2 / f1
+
+	e1 = (new.luminosity / comp.luminosity * f1 / f2)**2 * comp_ni_err**2
+
+	e2 = (comp.ni/comp.luminosity * f1 / f2)**2 * new.luminosity_err**2
+
+	e3 = (-comp.ni / comp.luminosity**2 * new.luminosity * f1 / f2)**2 * comp.luminosity_err**2
+
+	e4 = (-comp.ni *new.luminosity / comp.luminosity * f2 / f1**2 *(kA/(-tau_ni) * np.exp(-new.rise/tau_ni) + kB/(-tau_co)*np.exp(-new.rise/tau_co) - kB/(-tau_ni) * np.exp(-new.rise/tau_ni)))**2 * new.rise_err**2
+
+	e5 = (-comp.ni *new.luminosity / comp.luminosity * 1./f1 * (kA/(-tau_ni) * np.exp(-comp.rise/tau_ni) + kB/(-tau_co)*np.exp(-comp.rise/tau_co) - kB/(-tau_ni) * np.exp(-comp.rise/tau_ni)))**2 * comp.rise_err**2
+
+	#e5 = 0 for 06aj and 98bw as we have the XRF/GRB to time the explosion
+
+	err = np.sqrt(e1 + e2 + e3 + e4 + e5)
+
+	return (mass, err)
+        
 
 
 
@@ -112,7 +174,7 @@ def main():
     
     n_comp = np.size(lines) - 1
     
-    
+    final_vals = {'meject':[]}
     
 
 if __name__ == "__main__":
